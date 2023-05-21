efa4e9d276096a167090b4be4dcbb06bb714e47b
hollyhockberry <49336378+hollyhockberry@users.noreply.github.com>
2023-05-21 10:44:48 +0900

    プロジェクト ファイルを追加します。

---
+++ DataGridSample/Command.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace DataGridSample
+{
+    internal class Command : ICommand
+    {
+        readonly Action<object?> action;
+
+        public event EventHandler? CanExecuteChanged { add { } remove { } }
+
+        public Command(Action<object?> action) => this.action = action;
+
+        public bool CanExecute(object? _) => true;
+
+        public void Execute(object? parameter) => action(parameter);
+    }
+}
---
+++ DataGridSample/ViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Input;
+
+namespace DataGridSample
+{
+    internal class ViewModel : INotifyPropertyChanged
+    {
+        public DataTable? DataTable { get; private set; }
+
+        Command? _SelectDataCommand;
+
+        public ICommand SelectDataCommand => _SelectDataCommand ??= new((o) =>
+        {
+            var columns = 20 + (int.TryParse(o as string, out int i) ? i * 2: 0);
+
+            static IEnumerable<string> Columns(int n) => Enumerable
+            .Range(0, n)
+            .Select(_ => Path.GetFileNameWithoutExtension(Path.GetRandomFileName()));
+
+            DataTable = new();
+            DataTable.Columns.AddRange(Columns(columns).Select(n => new DataColumn(n)).ToArray());
+            Enumerable.Range(0, 10).Select(_ => DataTable.Rows.Add(Columns(columns).ToArray())).ToArray();
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(DataTable)));
+        });
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
