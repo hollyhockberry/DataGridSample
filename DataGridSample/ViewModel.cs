9c933dce08834d7d33f23ea546ef564b12dfc5b7
hollyhockberry <49336378+hollyhockberry@users.noreply.github.com>
2023-05-21 11:56:05 +0900

    ItemsSourceがnullにかわるパターンも追加

--- DataGridSample/ViewModel.cs
+++ DataGridSample/ViewModel.cs
@@ -15,16 +15,22 @@ namespace DataGridSample
 
         public ICommand SelectDataCommand => _SelectDataCommand ??= new((o) =>
         {
-            var columns = 20 + (int.TryParse(o as string, out int i) ? i * 2: 0);
+            if (o is string str)
+            {
+                var columns = 20 + (int.TryParse(str, out int i) ? i * 2 : 0);
 
-            static IEnumerable<string> Columns(int n) => Enumerable
-            .Range(0, n)
-            .Select(_ => Path.GetFileNameWithoutExtension(Path.GetRandomFileName()));
+                static IEnumerable<string> Columns(int n) => Enumerable
+                .Range(0, n)
+                .Select(_ => Path.GetFileNameWithoutExtension(Path.GetRandomFileName()));
 
-            DataTable = new();
-            DataTable.Columns.AddRange(Columns(columns).Select(n => new DataColumn(n)).ToArray());
-            Enumerable.Range(0, 10).Select(_ => DataTable.Rows.Add(Columns(columns).ToArray())).ToArray();
-
+                DataTable = new();
+                DataTable.Columns.AddRange(Columns(columns).Select(n => new DataColumn(n)).ToArray());
+                _ = Enumerable.Range(0, 10).Select(_ => DataTable.Rows.Add(Columns(columns).ToArray())).ToArray();
+            }
+            else
+            {
+                DataTable = null;
+            }
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(DataTable)));
         });
 
